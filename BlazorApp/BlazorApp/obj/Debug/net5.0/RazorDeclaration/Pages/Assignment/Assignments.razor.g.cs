4252fe2dc0345bfff919c9aa60cddc0e1babf096
Yassa Siddiqui <siddiquiyassa@gmail.com>
2022-04-04 15:36:06 +0100

    Fixed formatting errors

--- BlazorApp/BlazorApp/obj/Debug/net5.0/RazorDeclaration/Pages/Assignment/Assignments.razor.g.cs
+++ BlazorApp/BlazorApp/obj/Debug/net5.0/RazorDeclaration/Pages/Assignment/Assignments.razor.g.cs
@@ -13,98 +13,98 @@ namespace BlazorApp.Pages.Assignment
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\_Imports.razor"
+#line 1 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\_Imports.razor"
+#line 2 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\_Imports.razor"
+#line 3 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\_Imports.razor"
+#line 4 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\_Imports.razor"
+#line 5 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\_Imports.razor"
+#line 6 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\_Imports.razor"
+#line 7 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\_Imports.razor"
+#line 8 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\_Imports.razor"
+#line 9 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using BlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\_Imports.razor"
+#line 10 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using BlazorApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\Pages\Assignment\Assignments.razor"
+#line 2 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Pages\Assignment\Assignments.razor"
 using System.Security.Claims;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\Pages\Assignment\Assignments.razor"
+#line 3 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Pages\Assignment\Assignments.razor"
 using Microsoft.AspNetCore.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\Pages\Assignment\Assignments.razor"
+#line 5 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Pages\Assignment\Assignments.razor"
 using BlazorApp.Common.ViewModels;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\Pages\Assignment\Assignments.razor"
+#line 6 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Pages\Assignment\Assignments.razor"
 using BlazorApp.Services.Interface;
 
 #line default
@@ -119,7 +119,7 @@ using BlazorApp.Services.Interface;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 75 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp (2)\BlazorApp\Pages\Assignment\Assignments.razor"
+#line 75 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Pages\Assignment\Assignments.razor"
        
     private long idToDelete = 0;
     private long idToAdd = 0;
--- BlazorApp/BlazorApp/obj/Release/net5.0/RazorDeclaration/Pages/Assignment/AddAssignment.razor.g.cs
+++ BlazorApp/BlazorApp/obj/Release/net5.0/RazorDeclaration/Pages/Assignment/AddAssignment.razor.g.cs
@@ -13,98 +13,98 @@ namespace BlazorApp.Pages.Assignment
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\_Imports.razor"
+#line 1 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\_Imports.razor"
+#line 2 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\_Imports.razor"
+#line 3 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\_Imports.razor"
+#line 4 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\_Imports.razor"
+#line 5 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\_Imports.razor"
+#line 6 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\_Imports.razor"
+#line 7 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\_Imports.razor"
+#line 8 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\_Imports.razor"
+#line 9 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using BlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\_Imports.razor"
+#line 10 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using BlazorApp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\Pages\Assignment\AddAssignment.razor"
+#line 2 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Pages\Assignment\AddAssignment.razor"
 using System.Security.Claims;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\Pages\Assignment\AddAssignment.razor"
+#line 3 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Pages\Assignment\AddAssignment.razor"
 using Microsoft.AspNetCore.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\Pages\Assignment\AddAssignment.razor"
+#line 5 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Pages\Assignment\AddAssignment.razor"
 using BlazorApp.Common.ViewModels;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\Pages\Assignment\AddAssignment.razor"
+#line 6 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Pages\Assignment\AddAssignment.razor"
 using BlazorApp.Services.Interface;
 
 #line default
@@ -119,7 +119,7 @@ using BlazorApp.Services.Interface;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 44 "C:\Users\siddi\Downloads\BlazorApp updated\BlazorApp\BlazorApp\Pages\Assignment\AddAssignment.razor"
+#line 44 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Pages\Assignment\AddAssignment.razor"
        
     AssignmentVM assignmentVM = new AssignmentVM();
     public string UserName;
