5130d611cfe52e61b741491a6c8ebaee91fcfa9d
Yassa Siddiqui <siddiquiyassa@gmail.com>
2022-04-12 15:07:07 +0100

    fixed azure publishing error, i think

--- BlazorApp/BlazorApp/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ BlazorApp/BlazorApp/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace BlazorApp.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
+#line 1 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
+#line 2 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
+#line 3 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
+#line 4 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
+#line 5 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
+#line 6 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
+#line 7 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
+#line 8 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
+#line 9 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using BlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
+#line 10 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\_Imports.razor"
 using BlazorApp.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using BlazorApp.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 27 "C:\Users\siddi\source\repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Shared\NavMenu.razor"
+#line 27 "C:\Users\siddi\Source\Repos\yassa27\Degree-Classification-Project\BlazorApp\BlazorApp\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
